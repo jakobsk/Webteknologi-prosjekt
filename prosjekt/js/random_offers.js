76818f679830adb17377d87901b6b47c8eb4ccd4
Jakob Stenersen Kok <jakobskok95@gmail.com>
2019-11-05 17:55:08 +0100

    Fixed index mappings

--- prosjekt/js/random_offers.js
+++ prosjekt/js/random_offers.js
@@ -1,9 +1,9 @@
 
-var offers = [{name:"tennis1", price:"500", offer:"30%", picture: "../img/tennis1.jpg"}, {name:"tennis2", price:"500", offer:"30%", picture: "../img/tennis2.jpg"},
-{name:"run1", price:"500", offer:"30%", picture: "../img/run1.jpg"}, {name:"run2", price:"500", offer:"30%", picture: "../img/run2.jpg"},
-{name:"tennis3", price:"500", offer:"30%", picture: "../img/tennis3.jpg"}, {name:"tennis4", price:"500", offer:"30%", picture: "../img/tennis4.jpg"},
-{name:"tennis5", price:"500", offer:"30%", picture: "../img/tennis5.jpg"},{name:"fotball1", price:"500", offer:"30%", picture: "../img/fotballsko1.jpg"},
-{name:"fotball2", price:"500", offer:"30%", picture: "../img/fotballsko2.jpg"}, {name:"fotball4", price:"500", offer:"30%", picture: "../img/fotballsko4.jpg"}]
+var offers = [{name:"tennis1", price:"500", offer:"30%", picture: "../img/tennis1.jpg", id : 1}, {name:"tennis2", price:"500", offer:"30%", picture: "../img/tennis2.jpg", id : 2},
+{name:"run1", price:"500", offer:"30%", picture: "../img/run1.jpg", id : 3}, {name:"run2", price:"500", offer:"30%", picture: "../img/run2.jpg", id : 4},
+{name:"tennis3", price:"500", offer:"30%", picture: "../img/tennis3.jpg", id : 5}, {name:"tennis4", price:"500", offer:"30%", picture: "../img/tennis4.jpg", id : 6},
+{name:"tennis5", price:"500", offer:"30%", picture: "../img/tennis5.jpg", id : 7},{name:"fotball1", price:"500", offer:"30%", picture: "../img/fotballsko1.jpg", id : 8},
+{name:"fotball2", price:"500", offer:"30%", picture: "../img/fotballsko2.jpg", id : 9}, {name:"fotball4", price:"500", offer:"30%", picture: "../img/fotballsko4.jpg", id : 10}]
 var taken = [];
 
 function random(){
@@ -17,6 +17,7 @@ function random(){
 	
 	taken.push(i);
 	document.getElementById("r1").src= offers[i].picture;
+	document.getElementById("1").id = offers[i].id;
 	document.getElementById("r2").innerHTML= offers[i].name;
 	document.getElementById("r3").innerHTML= offers[i].offer;
 
@@ -30,6 +31,7 @@ function random(){
 	taken.push(i);
 	document.getElementById("r4").src= offers[i].picture;
 	document.getElementById("r5").innerHTML= offers[i].name;
+	document.getElementById("2").id = offers[i].id;
 	document.getElementById("r6").innerHTML= offers[i].offer;
 
 	for (var x = 0; x < 990; x++) {
@@ -40,9 +42,8 @@ function random(){
 	}
 	taken.push(i);
 	document.getElementById("r7").src= offers[i].picture;
+	document.getElementById("3").id = offers[i].id;
 	document.getElementById("r8").innerHTML= offers[i].name;
 	document.getElementById("r9").innerHTML= offers[i].offer;
-
-
 }
 
