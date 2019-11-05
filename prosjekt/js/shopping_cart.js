6e4dd40214f1b8feac6ee4a5f6bf7c48a3dddc1e
Jakob Stenersen Kok <jakobskok95@gmail.com>
2019-11-05 16:40:44 +0100

    fixed bug

--- prosjekt/js/shopping_cart.js
+++ prosjekt/js/shopping_cart.js
@@ -1,66 +1,66 @@
 var database = {
   item_1 : {
-    id = 1,
-    name = "Random navn",
-    picture = "../img/fotballsko1.jpg",
-    price = "100kr",
-    description = "En fake tekst om produktet"
-  }
+    id : 1,
+    name : "Random navn",
+    picture : "../img/fotballsko1.jpg",
+    price : "100kr",
+    description : "En fake tekst om produktet"
+  },
   item_2 : {
-    id = 2,
-    name = "Random navn",
-    picture = "../img/fotballsko2.jpg",
-    price = "100kr",
-    description = "En fake tekst om produktet"
-  }
+    id : 2,
+    name : "Random navn",
+    picture : "../img/fotballsko2.jpg",
+    price : "100kr",
+    description : "En fake tekst om produktet"
+  },
   item_3 : {
-    id = 3,
-    name = "Random navn",
-    picture = "../img/fotballsko3.jpg",
-    price = "100kr",
-    description = "En fake tekst om produktet"
-  }
+    id : 3,
+    name : "Random navn",
+    picture : "../img/fotballsko3.jpg",
+    price : "100kr",
+    description : "En fake tekst om produktet"
+  },
   item_4 : {
-    id = 4,
-    name = "Random navn",
-    picture = "../img/fotballsko1.jpg",
-    price = "100kr",
-    description = "En fake tekst om produktet"
-  }
+    id : 4,
+    name : "Random navn",
+    picture : "../img/fotballsko1.jpg",
+    price : "100kr",
+    description : "En fake tekst om produktet"
+  },
   item_5 : {
-    id = 5,
-    name = "Random navn",
-    picture = "../img/fotballsko1.jpg",
-    price = "100kr",
-    description = "En fake tekst om produktet"
-  }
+    id : 5,
+    name : "Random navn",
+    picture : "../img/fotballsko1.jpg",
+    price : "100kr",
+    description : "En fake tekst om produktet"
+  },
   item_6 : {
-    id = 6,
-    name = "Random navn",
-    picture = "../img/fotballsko1.jpg",
-    price = "100kr",
-    description = "En fake tekst om produktet"
-  }
+    id : 6,
+    name : "Random navn",
+    picture : "../img/fotballsko1.jpg",
+    price : "100kr",
+    description : "En fake tekst om produktet"
+  },
   item_7 : {
-    id = 7,
-    name = "Random navn",
-    picture = "../img/fotballsko1.jpg",
-    price = "100kr",
-    description = "En fake tekst om produktet"
-  }
+    id : 7,
+    name : "Random navn",
+    picture : "../img/fotballsko1.jpg",
+    price : "100kr",
+    description : "En fake tekst om produktet"
+  },
   item_8 : {
-    id = 8,
-    name = "Random navn",
-    picture = "../img/fotballsko1.jpg",
-    price = "100kr",
-    description = "En fake tekst om produktet"
-  }
+    id : 8,
+    name : "Random navn",
+    picture : "../img/fotballsko1.jpg",
+    price : "100kr",
+    description : "En fake tekst om produktet"
+  },
   item_9 : {
-    id = 9,
-    name = "Random navn",
-    picture = "../img/fotballsko1.jpg",
-    price = "100kr",
-    description = "En fake tekst om produktet"
+    id : 9,
+    name : "Random navn",
+    picture : "../img/fotballsko1.jpg",
+    price : "100kr",
+    description : "En fake tekst om produktet"
   }
 }
 
