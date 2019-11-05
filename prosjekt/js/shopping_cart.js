db1ab4195fba89bd6cedf2a67ecb855f4ab1af4f
Jakob Stenersen Kok <jakobskok95@gmail.com>
2019-11-05 16:35:14 +0100

    Added a fake database

--- prosjekt/js/shopping_cart.js
+++ prosjekt/js/shopping_cart.js
@@ -1,3 +1,69 @@
+var database = {
+  item_1 : {
+    id = 1,
+    name = "Random navn",
+    picture = "../img/fotballsko1.jpg",
+    price = "100kr",
+    description = "En fake tekst om produktet"
+  }
+  item_2 : {
+    id = 2,
+    name = "Random navn",
+    picture = "../img/fotballsko2.jpg",
+    price = "100kr",
+    description = "En fake tekst om produktet"
+  }
+  item_3 : {
+    id = 3,
+    name = "Random navn",
+    picture = "../img/fotballsko3.jpg",
+    price = "100kr",
+    description = "En fake tekst om produktet"
+  }
+  item_4 : {
+    id = 4,
+    name = "Random navn",
+    picture = "../img/fotballsko1.jpg",
+    price = "100kr",
+    description = "En fake tekst om produktet"
+  }
+  item_5 : {
+    id = 5,
+    name = "Random navn",
+    picture = "../img/fotballsko1.jpg",
+    price = "100kr",
+    description = "En fake tekst om produktet"
+  }
+  item_6 : {
+    id = 6,
+    name = "Random navn",
+    picture = "../img/fotballsko1.jpg",
+    price = "100kr",
+    description = "En fake tekst om produktet"
+  }
+  item_7 : {
+    id = 7,
+    name = "Random navn",
+    picture = "../img/fotballsko1.jpg",
+    price = "100kr",
+    description = "En fake tekst om produktet"
+  }
+  item_8 : {
+    id = 8,
+    name = "Random navn",
+    picture = "../img/fotballsko1.jpg",
+    price = "100kr",
+    description = "En fake tekst om produktet"
+  }
+  item_9 : {
+    id = 9,
+    name = "Random navn",
+    picture = "../img/fotballsko1.jpg",
+    price = "100kr",
+    description = "En fake tekst om produktet"
+  }
+}
+
 
 function addToCart(id){
   var objekt_id =  parseInt(id);
@@ -31,6 +97,6 @@ function getCart(){
 
 function deleteItem(id){
   var ul = document.getElementById('shoppingcart_list');
-  console.log('slett');
+  console.log('slett element');
 
 }
