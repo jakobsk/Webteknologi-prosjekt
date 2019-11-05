c34e5f1ea84989f7fdfb6f1ee721bd8fabe23faa
Torgeir Laurvik <torgeir.gutt@hotmail.com>
2019-11-05 15:33:49 +0100

    add shoppingcart func

--- prosjekt/js/shopping_cart.js
+++ prosjekt/js/shopping_cart.js
@@ -1,3 +1,26 @@
+var cart = ['Hest','Fisk'];
 function addToCart(id){
-  var content =  document.getElementById(id).
+  console.log('Denne blir aktivert');
+  var objekt_id =  id;
+  cart.push(objekt_id);
+  console.log(cart);
+}
+function getCart(){
+  console.log(cart);
+  var ul = document.getElementById('shoppingcart_list');
+  for (var i = 0; i < cart.length; i++) {
+    var li = document.createElement('LI');
+    var text = document.createTextNode(cart[i]+'   ');
+    var button = document.createElement("button");
+    button.innerText = "Delete this";
+    button.onclick = deleteItem(this.id);
+    li.appendChild(text);
+    li.appendChild(button);
+    ul.appendChild(li);
+  }
+}
+function deleteItem(id){
+  var ul = document.getElementById('shoppingcart_list');
+  console.log('slett');
+
 }
